57bc401e7ca22f2cb6cde5aae282ee3946297fea
JealousMagician13 <77805708+JealousMagician13@users.noreply.github.com>
2024-08-27 17:00:37 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-X = [[30],[40],[50],[60],[20],[10],[70]]
-y = [0,1,1,1,0,0,1]
+X = [[30],[40],[50],[60],[20],[10],[70], [80]]
+y = [0,1,1,1,0,0,1,1]
 classifier = LogisticRegression()
 classifier.fit(X,y)
 X_marks=[[20]]
